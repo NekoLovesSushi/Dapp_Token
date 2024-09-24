7d563103129a859d428ae2305acaa0ed3c3431f2
Eleanor <eleanor.stein777@gmail.com>
2024-09-24 15:04:47 -0400

    8. Token Deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
-/** @type import('hardhat/config').HardhatUserConfig */
+const privateKeys = process.env.PRIVATE_KEYS || ""
+
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    localhost: {},
+    localhost: {}, 
+    sepolia: {
+      url:`https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKeys.split(",")
+    }
   },
+  etherscan: {
+    apiKey: {
+      sepolia: process.env.ETHERSCAN_API_KEY
+    }
+  }
 };
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,9 +1,11 @@
+const { ethers } = require('hardhat');
+
 async function main() {
   // Fetch contract to deploy
   const Token = await ethers.getContractFactory("Token");
 
   // Deploy contract
-  const token = await Token.deploy();
+  const token = await Token.deploy('Dapp University', 'DAPP', '1000000');
   await token.deployed();
   console.log(`Token Deployed to: ${token.address}`);
 }
